abe1497854aa40132cc65981c987001ae907b567
Christopher Guan <cwguan@ucsd.edu>
2018-03-05 19:14:26 -0800

    Implement Retweet/Favorite and their undos; need icons

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -136,6 +136,20 @@ class APIManager: SessionManager {
     
     
     // MARK: TODO: Un-Favorite a Tweet
+    func unfavorite(_ tweet: Tweet, completion: @escaping (Tweet?, Error?) -> ()) {
+        let urlString = "https://api.twitter.com/1.1/favorites/destroy.json"
+        let parameters = ["id": tweet.id]
+        request(urlString, method: .post, parameters: parameters, encoding: URLEncoding.queryString).validate().responseJSON { (response) in
+            if response.result.isSuccess,
+                let tweetDictionary = response.result.value as? [String: Any] {
+                let tweet = Tweet(dictionary: tweetDictionary)
+                completion(tweet, nil)
+            } else {
+                completion(nil, response.result.error)
+            }
+        }
+    }
+    
     
     // MARK: TODO: Retweet
     func retweet(_ tweet: Tweet, completion: @escaping (Tweet?, Error?) -> ()) {
@@ -152,7 +166,22 @@ class APIManager: SessionManager {
         }
     }
     
+    
     // MARK: TODO: Un-Retweet
+    func unretweet(_ tweet: Tweet, completion: @escaping (Tweet?, Error?) -> ()) {
+        let urlString = "https://api.twitter.com/1.1/statuses/unretweet/" + String(tweet.id) + ".json"
+        let parameters = ["id": tweet.id]
+        request(urlString, method: .post, parameters: parameters, encoding: URLEncoding.queryString).validate().responseJSON { (response) in
+            if response.result.isSuccess,
+                let tweetDictionary = response.result.value as? [String: Any] {
+                let tweet = Tweet(dictionary: tweetDictionary)
+                completion(tweet, nil)
+            } else {
+                completion(nil, response.result.error)
+            }
+        }
+    }
+    
     
     // MARK: TODO: Compose Tweet
     
--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -18,11 +18,27 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var retweetCountLabel: UILabel!
     @IBOutlet weak var favoriteCountLabel: UILabel!
+
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
     
     var tweet: Tweet! {
         didSet {
             tweetTextLabel.text = tweet.text
+            
+            if (tweet.retweeted == true) {
+                retweetButton.setTitle("Un-Retweet", for: .normal)
+            } else {
+                retweetButton.setTitle("Retweet", for: .normal)
+            }
             retweetCountLabel.text = String(tweet.retweetCount)
+            
+            
+            if (tweet.favorited == true) {
+                favoriteButton.setTitle("Un-Favorite", for: .normal)
+            } else {
+                favoriteButton.setTitle("Favorite", for: .normal)
+            }
             favoriteCountLabel.text = String(tweet.favoriteCount!)
             
             tweeterNameLabel.text = tweet.user.name
@@ -38,40 +54,63 @@ class TweetCell: UITableViewCell {
     
     @IBAction func onRetweet(_ sender: Any) {
         if (tweet.retweeted != true) {
-            tweet.retweeted = true
-            tweet.retweetCount += 1
-            
             APIManager.shared.retweet(tweet) { (tweet: Tweet?, error: Error?) in
                 if let  error = error {
-                    print("Error favoriting tweet: \(error.localizedDescription)")
+                    print("Error retweeting tweet: \(error.localizedDescription)")
                 } else if let tweet = tweet {
                     print("Successfully retweeted the following Tweet: \n\(tweet.text)")
+                    self.tweet.retweeted = true
+                    self.tweet.retweetCount += 1
+                    self.retweetButton.setTitle("Un-Retweet", for: .normal)
+                    self.refreshData()
                 }
             }
-            refreshData()
             
         } else {
-            print("Already retweeted")
+            APIManager.shared.unretweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error un-retweeting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully un-retweeted the following Tweet: \n\(tweet.text)")
+                    self.tweet.retweeted = false
+                    self.tweet.retweetCount -= 1
+                    self.retweetButton.setTitle("Retweet", for: .normal)
+                    self.refreshData()
+                }
+            }
         }
+        self.refreshData()
     }
     
     
     @IBAction func onFavorite(_ sender: Any) {
         if (tweet.favorited != true) {
-            tweet.favorited = true
-            tweet.favoriteCount! += 1
-            
             APIManager.shared.favorite(tweet) { (tweet: Tweet?, error: Error?) in
                 if let  error = error {
                     print("Error favoriting tweet: \(error.localizedDescription)")
                 } else if let tweet = tweet {
                     print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                    self.tweet.favorited = true
+                    self.tweet.favoriteCount! += 1
+                    self.favoriteButton.setTitle("Unfavorite", for: .normal)
+                    self.refreshData()
                 }
             }
-            refreshData()
+            
         } else {
-            print("Already favorited")
+            APIManager.shared.unfavorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error unfavoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully unfavorited the following Tweet: \n\(tweet.text)")
+                    self.tweet.favorited = false
+                    self.tweet.favoriteCount! -= 1
+                    self.favoriteButton.setTitle("Favorite", for: .normal)
+                    self.refreshData()
+                }
+            }
         }
+        self.refreshData()
     }
     
     
