b50f3b3bac9824ab3ab9d4d67fec714546340dc4
Christopher Guan <cwguan@ucsd.edu>
2018-03-06 17:53:03 -0800

    Implement favorite and retweet in DetailView

--- twitter_alamofire_demo/DetailViewController.swift
+++ twitter_alamofire_demo/DetailViewController.swift
@@ -28,6 +28,72 @@ class DetailViewController: UIViewController {
     var tweet: Tweet!
     
     
+    @IBAction func onReply(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onRetweet(_ sender: Any) {
+        if (tweet.retweeted != true) {
+            APIManager.shared.retweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error retweeting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully retweeted the following Tweet: \n\(tweet.text)")
+                    self.tweet.retweeted = true
+                    self.tweet.retweetCount += 1
+                    self.retweetButton.setImage(#imageLiteral(resourceName: "retweet-icon-green"), for: .normal)
+                    self.refreshData()
+                }
+            }
+            
+        } else {
+            APIManager.shared.unretweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error un-retweeting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully un-retweeted the following Tweet: \n\(tweet.text)")
+                    self.tweet.retweeted = false
+                    self.tweet.retweetCount -= 1
+                    self.retweetButton.setImage(#imageLiteral(resourceName: "retweet-icon"), for: .normal)
+                    self.refreshData()
+                }
+            }
+        }
+        self.refreshData()
+    }
+    
+    
+    @IBAction func onFavorite(_ sender: Any) {
+        if (tweet.favorited != true) {
+            APIManager.shared.favorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                    self.tweet.favorited = true
+                    self.tweet.favoriteCount! += 1
+                    self.favoriteButton.setImage(#imageLiteral(resourceName: "favor-icon-red"), for: .normal)
+                    self.refreshData()
+                }
+            }
+            
+        } else {
+            APIManager.shared.unfavorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error unfavoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully unfavorited the following Tweet: \n\(tweet.text)")
+                    self.tweet.favorited = false
+                    self.tweet.favoriteCount! -= 1
+                    self.favoriteButton.setImage(#imageLiteral(resourceName: "favor-icon"), for: .normal)
+                    self.refreshData()
+                }
+            }
+        }
+        self.refreshData()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -58,6 +124,13 @@ class DetailViewController: UIViewController {
 
     }
 
+    
+    func refreshData() {
+        retweetCountLabel.text = String(tweet.retweetCount)
+        favoriteCountLabel.text = String(tweet.favoriteCount!)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
